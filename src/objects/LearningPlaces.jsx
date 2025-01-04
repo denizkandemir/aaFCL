ee7a155c1423a2496c8162d8cc60b10471bab7f7
Deniz Kandemir <denizkandemir137@gmail.com>
2025-01-04 19:32:16 +0300

    learning places texts added

--- src/objects/LearningPlaces.jsx
+++ src/objects/LearningPlaces.jsx
@@ -29,8 +29,8 @@ const LearningPlaces = [
         svg:cardImg1,
         text:"Öğrencilerin fikirlerini özgürce ifade ettiği, tartışmalar ve aktif katılımla öğrenmeyi destekleyen bir iletişim merkezi.",
         title:"Etkileşim Alanı",
-        pageText:"Etkileşim alanında öğrencilerimiz farklı teknolojileri kullanarak öğrenme sürecine aktif olarak katlırlar.​Etkileşim alanında; akıllı tahta,açık eğitim kaynakları,interaktif öğrenme içerikleri,tabletler,ses sitemi,zeka ve strateji eğitim seti,VR Box,JBL Hoparlör,Kişisel akıllı telefonlar kullanılmaktadır.",
-        pageText2:"Etkileşim alanında öğrencilerimiz farklı teknolojileri kullanarak öğrenme sürecine aktif olarak katlırlar.​Etkileşim alanında; akıllı tahta,açık eğitim kaynakları,interaktif öğrenme içerikleri,tabletler,ses sitemi,zeka ve strateji eğitim seti,VR Box,JBL Hoparlör,Kişisel akıllı telefonlar kullanılmaktadır.",
+        pageText:"Öğretmenler öğrenci katılımını ve etkileşimini arttırmak için teknolojiyi kullanırlar. Geleneksel öğrenme ortamlarında tüm öğrencilerin aktif olarak katılımının sağlanması aşılması gereken bir zorluktur. Çözümler tabletler ve akıllı telefonlar gibi bireysel cihazlarla, interaktif beyaz tahta ve interaktif öğrenme içerikleri arasında değişiklik gösterir. ",
+        pageText2:"Etkileşim alanı öğrenmede hem öğrencilerin hem de öğretmenlerin aktif katılımlarını gerektirir. Sınıfımızın etkileşim alanında yeşil perde, ses ve görüntü kaydedici cihaz ve tabureler bulunmaktadır.",
     },
     {
         id:2,
@@ -48,8 +48,8 @@ const LearningPlaces = [
         svg:cardImg5,
         text:"Yaratıcı fikirlerin projelere dönüştüğü, yenilikçi düşüncenin ve problem çözme becerilerinin öne çıktığı bir öğrenme ortamı.",
         title:"Geliştirme Alanı",
-        pageText:"Geliştirme alanında öğrencilerimiz kendi öğrenme hızlarına uygun ve teknolojileri kullanarak öğrenme sürecine aktif olarak katılırlar.Geliştirme alanında çalışma köşesi,(zemin halısı,armut koltuklar,sehpa )mini kütüphane (Türkçe  ve İngilizce kitaplar,dergiler,zeka ve strateji oyun seti,tablet ve akıllı telefonlar bulunmaktadır.",
-        pageText2:"Geliştirme alanında öğrencilerimiz kendi öğrenme hızlarına uygun ve teknolojileri kullanarak öğrenme sürecine aktif olarak katılırlar.Geliştirme alanında çalışma köşesi,(zemin halısı,armut koltuklar,sehpa )mini kütüphane (Türkçe  ve İngilizce kitaplar,dergiler,zeka ve strateji oyun seti,tablet ve akıllı telefonlar bulunmaktadır.",
+        pageText:"İnformal öğrenme ve kendini yansıtma (self-reflection) için bir alandır. Öğrenci çalışmasını bağımsız olarak kendi hızında yapabilir. Ayrıca öğrenciler okulda ve evde formal sınıf düzenlemelerinin dışında kendi ilgi alanlarına yoğunlaşarak informal öğrenmeler gerçekleştirebilir. ",
+        pageText2:"Öz yönelimli öğrenmeyi güçlendirecek yollar sunarak okul, öğrenenlerin kendini yansıtma ve üst bilişsel becerilerini destekler. Sınıfımızın geliştirme alanında 1 adet ayrılan altıgen masa bireysel çalışma için ayrılmıştır. 6 adet tabure ve kitaplar bulunmaktadır.",
 
     },
 
@@ -69,8 +69,8 @@ const LearningPlaces = [
         svg:cardImg4,
         text:"Bilgiye ulaşma becerilerini geliştiren, merak edilen konuların derinlemesine incelendiği modern bir keşif alanı.",
         title:"Araştırma Alanı",
-        pageText:"Araştırma alanında öğrencilerimiz bilgi ve veri okuryazarlığı yeterlik düzeyi bağlamında online ortamlarda araştırma yaparlar, kendileri için ihtiyaç duydukları veriye, bilgiye veya dijital içeriklere nasıl erişebileceklerini; elde ettikleri içeriklere ve bilginin kaynağına eleştirel bakmayı ve bilgiyi kendi çalışmaları için nasıl organize edip kullanabileceklerini öğrenirler. ",
-        pageText2:"​Araştırma alanında bilgsayarlar,yazıcı,tabletler,3D yazıcı, drone, teleskop, robot masası, mbot, 3 adet arduino eğitim seti,STEM tasarım ve üretim ünitesi, yenilenebilir güneş enerji deney seti, rüzgar enerjisi deney seti bulunmaktadır.",
+        pageText:"Öğrenciler kendilerini keşfetmek için teşvik edilirler; onlara pasif dinleyiciler olmaları yerine aktif katılımcı olmaları için fırsatlar verilir. Araştırma alanında, öğretmenler öğrencilerin kritik düşünme becerilerini geliştirmek için sorgulamaya dayalı ve proje tabanlı öğrenme fırsatları sunar. ",
+        pageText2:"​Sınıfımızın araştırma alanında, 4 bilgisayar, bilgisayar masaları, 4 sandalye, 1 robotik kodlama masası (FFL standartlarına uygun), 5 adet Bee Bot, 2 Adet Matatalab, 1 Adet Ozobot, 6 Adet Cody Rocky, 6 Adet M bot, 2 Adet 4D kartlar, 1 Adet mikroskop, akıl ve zeka oyunları bulunmaktadır.",
 
     },
 
@@ -90,8 +90,8 @@ const LearningPlaces = [
         svg:cardImg2,
         text:"Düşüncelerin somut sonuçlara dönüştüğü, uygulamalı çalışmalara olanak sağlayan bir tasarım ve üretim alanı.",
         title:"Üretim Alanı",
-        pageText:"Üretim alanında; öğrencilerimiz bağımsız hareket ederek hayatla bağlantılı problem durumları üzerine çalışmalar planlamakta, takım çalışması eşliğinde ve  modern teknolojileri kullanarak somut ürünler ortaya koymaktadırlar.Üretim alanında; yeşil perde, softbox ışık seti,2 dijital kamera, video düzenleme yazılımı, müzik enstrümanları (bağlama,gitar,piano,davul,org …)gibi araçlar, kaynaklar bulunmaktadır.",
-        pageText2:"Üretim alanında; öğrencilerimiz bağımsız hareket ederek hayatla bağlantılı problem durumları üzerine çalışmalar planlamakta, takım çalışması eşliğinde ve  modern teknolojileri kullanarak somut ürünler ortaya koymaktadırlar.Üretim alanında; yeşil perde, softbox ışık seti,2 dijital kamera, video düzenleme yazılımı, müzik enstrümanları (bağlama,gitar,piano,davul,org …)gibi araçlar, kaynaklar bulunmaktadır.",
+        pageText:"Üretim alanında, öğrenciler gerçek bilginin inşası etkinlikleri ile çalışırlar. Yorumlama, analiz, grup çalışmaları ve değerlendirme üretim sürecinin önemli parçalarıdır. Yaparak yaşayarak öğrenmenin hayata geçirildiği, öğrencilerin proje tabanlı öğrenme ile ince becerilerinin geliştirildiği alandır. ",
+        pageText2:"Sınıfımızın üretim alanında bir bilgisayar, bir 3D yazıcı ve 3D yazıcıya ait parçaları depolamak adına sürgülü dolap ve tabureler bulunmaktadır.",
 
     },
 
@@ -111,8 +111,8 @@ const LearningPlaces = [
         svg:cardImg3,
         text:"Sunum alanı, öğrencilerin projelerini sergileyerek kendilerini ifade etme fırsatı buldukları bir sahne ortamıdır.",
         title:"Sunum Alanı",
-        pageText:"Sunum alanında öğrencilerimiz ürettikleri çalışmaları sunarlar. Özellikle online kaynakları etik çerçevede ve e-güvenliğe dikkat edilerek aktif olarak kullanırlar  ve  çalışmalarını dijital ortamlarda sunarlar; diğer öğrencilerle etkileşim kurarlar geri bildirim alırlar. Drama, tartışma ve panel etkinliklerimizi bu alanda yapılmaktadır.",
-        pageText2:"Ayrıca bu alan dahilinde 'Uzay Teması' çalışmamız devam etmektedir.Sunum alanında esnek tribün oturma düzeni, bilgisayar, HD projeksiyon, projeksiyon perdesi, ses sistemi, interaktif online araçlar,(Web 2.0,log,websitesi)karaoke mikrofon ve 3 boyutlu (ingilizce) ahşap dünya haritası bulunmaktadır.",
+        pageText:"Öğrenciler, çalışmalarını sunmak, paylaşmak ve geri bildirim almak için çeşitli araçlara ve becerilere ihtiyaç duyarlar. Bu süreç, yalnızca öğrencilerin bireysel gelişimlerine katkıda bulunmakla kalmaz, aynı zamanda onların iletişim ve iş birliği becerilerini geliştirmelerine de olanak tanır. Çalışmaların sunumu, öğrencilerin yaptıkları işleri yalnızca sergilemekle sınırlı kalmayıp, aynı zamanda diğer öğrenciler, öğretmenler ve hatta geniş bir kitle ile etkileşim kurmalarını sağlar",
+        pageText2:"Etkileşimli tahta, öğrencilerin görsel ve dijital içeriklerini etkili bir şekilde sunmalarına olanak tanırken, sunum köşesi ise onların kendilerini özgürce ifade edebilecekleri bir platform sunar. Minderler ve tabureler ise, diğer öğrencilerin bu sunumları rahat bir şekilde dinlemelerine ve aktif bir şekilde katılmalarına olanak tanıyan bir ortam oluşturur.",
 
     },
 
@@ -132,8 +132,8 @@ const LearningPlaces = [
         svg:cardImg6,
         text: "Ekip çalışmasını teşvik eden, ortak hedeflere ulaşmak için birlikte çalışmayı destekleyen paylaşım odaklı bir ortam.",
         title:"İşbirliği Alanı",
-        pageText:"İşbirliği alanında öğrencilerimiz modern teknolojileri ve online dijital ortamları ve web 2. araçlarını kullanarak  işbirliği sürecine aktif olarak katılırlar. Sınıfımızda yapılan çalışmalar “ Harmanlamış Öğrenme” yaklaşımına  uygun olduğu için öğrenciler okul dışında da işbirliğini dijital ortamlarda sürdürürler.",
-        pageText2:" Ayrıca bu alanda doğa teması çalışması(Eko-Bahçe) hazırlığı devam etmektedir.İşbirliği alanında portatif beyaz tahta, işbirlikçi  esnek masalar, tabletler, online sunum, zihin haritalama, anket  e-kitap ve interaktif oyun uygulamalar, sanal ve arttırılmış gerçeklik uygulamaları,eko-bahçe mevcuttur.",
+        pageText:"Öğrenme alanlarında başkaları ile işbirliği içinde çalışmak çok önemlidir. Araştırma, üretim ve sunum çalışmaları boyunca takım çalışmaları gerçekleştirilir. İşbirliğinin kalitesini grup içindeki aidiyet duygusu, sorumluluk paylaşımı ve karar verme süreci belirler. Oyunla ve interaktif öğrenme ile öğrenmenin kalıcı olması hedeflenmektedir.",
+        pageText2:"Sınıfımızın işbirliği alanında 1 altıgen ayrılıp birleşen masa, 6 adet renkli tabureler bulunmaktadır.",
   
     },
     
