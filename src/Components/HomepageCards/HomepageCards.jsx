828a755bd7898bdc13d75dd15727f1f320d1b7ea
Deniz Kandemir <denizkandemir137@gmail.com>
2025-01-11 02:44:03 +0300

    button animations added

--- src/Components/HomepageCards/HomepageCards.jsx
+++ src/Components/HomepageCards/HomepageCards.jsx
@@ -39,10 +39,36 @@ const HomepageCards = () => {
                                             className="place-link"
                                             to={{
                                                 pathname: `${place.path}/${place.id}`,
-                                              }}
-                                        >                
-                                            <button className="place-button">
-                                            <p className="button-text"> İncele </p> </button>
+                                            }}
+                                        >
+                                            <div className="cards-button-container">
+
+                                            <div className="cards-buttons">
+                                                <button className="cards-blob-btn">
+                                                    İletişim
+                                                    <span className="cards-blob-btn__inner">
+                                                        <span className="cards-blob-btn__blobs">
+                                                            <span className="cards-blob-btn__blob"></span>
+                                                            <span className="cards-blob-btn__blob"></span>
+                                                            <span className="cards-blob-btn__blob"></span>
+                                                            <span className="cards-blob-btn__blob"></span>
+                                                        </span>
+                                                    </span>
+                                                </button>
+                                                <br />
+
+                                                <svg height={0} xmlns="http://www.w3.org/2000/svg" version="1.1">
+                                                    <defs>
+                                                        <filter id="goo">
+                                                            <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+                                                            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
+                                                            <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+                                                        </filter>
+                                                    </defs>
+                                                </svg>
+                                            </div>
+                                            </div>
+
                                         </Link>
                                     </div>
                                 </div>
--- src/Components/HomepageContact/HomePageContact.jsx
+++ src/Components/HomepageContact/HomePageContact.jsx
@@ -9,21 +9,43 @@ const HomePageContact = () => {
 
     withFadeInOnScroll();
 
-    return(
+    return (
         <div className="home-contact-container ">
             <div className="home-contact-content-container fade-in">
                 <img src={school} alt="" className="home-contact-img" />
                 <div className="home-contact-text-container">
                     <h3 className="home-contact-title"> FCL Sınıfımızı Ziyaret Edin! </h3>
                     <p className="home-contact-second-title"> Detaylı Bilgi Almak ve Sınıfımızı Görmek İçin Okulumuz İle İletişime Geçin. </p>
-                    <p className="home-contact-p"> 
-                      Okulumuzu ziyaret ederek, FCL sınıfımızın sağladığı imkanları yerinde görebilir, sunduğumuz eğitim yöntemleri hakkında daha fazla bilgi edinebilirsiniz. Uzman kadromuz, sizleri ağırlamaktan mutluluk duyacak ve sorularınızı yanıtlamaya hazır olacaktır. </p>
+                    <p className="home-contact-p">
+                        Okulumuzu ziyaret ederek, FCL sınıfımızın sağladığı imkanları yerinde görebilir, sunduğumuz eğitim yöntemleri hakkında daha fazla bilgi edinebilirsiniz. Uzman kadromuz, sizleri ağırlamaktan mutluluk duyacak ve sorularınızı yanıtlamaya hazır olacaktır. </p>
                     <Link to={"/iletişim"} className="home-contact-link">
-                      <button className="home-contact-button"> 
-                      <p className="button-p">
-                      İletişim
-                        </p>    
-                     </button>
+                        <div className="homepage-contact-button-container">
+                            <div className="homepage-contact-buttons">
+                                <button className="homepage-contact-btn">
+                                    İletişim
+                                    <span className="homepage-contact-btn__inner">
+                                        <span className="homepage-contact-btn__blobs">
+                                            <span className="homepage-contact-btn__blob"></span>
+                                            <span className="homepage-contact-btn__blob"></span>
+                                            <span className="homepage-contact-btn__blob"></span>
+                                            <span className="homepage-contact-btn__blob"></span>
+                                        </span>
+                                    </span>
+                                </button>
+                                <br />
+
+                                <svg height={0} xmlns="http://www.w3.org/2000/svg" version="1.1">
+                                    <defs>
+                                        <filter id="goo">
+                                            <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+                                            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
+                                            <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+                                        </filter>
+                                    </defs>
+                                </svg>
+                            </div>
+                        </div>
+
                     </Link>
                 </div>
             </div>
