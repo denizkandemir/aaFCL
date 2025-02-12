906d02d4a852c52d1507b9a3f72d8e34cddde8a4
Deniz Kandemir <denizkandemir137@gmail.com>
2025-02-12 14:20:40 +0300

    admin page button icons added

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -2,6 +2,8 @@ import "./Admin.scss";
 import Events from "../../objects/Events";
 import fclLogo from "/fcl-logo.png";
 import withFadeInOnScroll from "../../hooks/animation/Animation";
+import editIcon from "/edit.png";
+import deleteIcon from "/delete.png";
 
 const Admin = () => {
 
@@ -9,36 +11,39 @@ const Admin = () => {
 
     return (
         <>
-            <div className="admin-events-container">
-                <div className="event-cards-data-container">
+            <div className="admin-admins-container">
+                <div className="admin-cards-data-container">
                     {
-                        Events.slice().reverse().map((event) => (
-                            <div key={event.id} className="event-ınfo-card-wrapper fade-in">
-                                <div className="event-ınfo-img-container">
-                                    <img src={event.cardImg} className="event-ınfo-info-img" alt="" />
+                        Events.slice().reverse().map((admin) => (
+                            <div key={admin.id} className="admin-ınfo-card-wrapper fade-in">
+                                <div className="admin-ınfo-img-container">
+                                    <img src={admin.cardImg} className="admin-ınfo-info-img" alt="" />
                                 </div>
-                                <div className="event-ınfo-content-container">
-                                    <div className="event-ınfo-text-container">
-                                        <p className="event-ınfo-p"> {event.title} </p>
-                                        <p className="event-ınfo-text"> {event.text} </p>
+                                <div className="admin-ınfo-content-container">
+                                    <div className="admin-ınfo-text-container">
+                                        <p className="admin-ınfo-p"> {admin.title} </p>
+                                        <p className="admin-ınfo-text"> {admin.text} </p>
                                     </div>
-                                    <div className="event-ınfo-button-container">
-                                        <div className="event-ınfo-logo-container">
+                                    <div className="admin-ınfo-button-container">
+                                        <div className="admin-ınfo-logo-container">
                                             <div className="fcl-logo-texts-container">
-                                                <img className="event-ınfo-logo" src={fclLogo} alt="" />
+                                                <img className="admin-ınfo-logo" src={fclLogo} alt="" />
                                                 <div className="fcl-texts-container">
                                                     <p className="fcl-text"> Future <br /> Classroom Lab </p>
                                                     <p className="fcl-bottom-text"> By European Schoolnet </p>
                                                 </div>
                                             </div>
                                         </div>
-
+                                        <div className="admin-icons-container">
+                                            <img src={editIcon} alt="" className="admin-icon-img" />
+                                            <img src={deleteIcon} alt="" className="admin-icon-img" />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         ))
                     }
-                    <div className="add-event-container">
+                    <div className="add-admin-container">
 
                     </div>
                 </div>
