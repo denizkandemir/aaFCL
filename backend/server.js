17363a0823a7c18887b984d7226f3e957d62b0d6
Deniz Kandemir <denizkandemir137@gmail.com>
2025-03-01 23:58:56 +0300

    database connection files added

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+require("dotenv").config();
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+
+// Server Listening
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
