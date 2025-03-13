97621307d9db98550c02416c21142f858b552887
Deniz Kandemir <denizkandemir137@gmail.com>
2025-03-13 12:25:24 +0300

    admin backend codes added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,17 @@
 const express = require("express");
-const admin = require("../models/admin"); 
+const User = require("../models/admin"); // Ensure correct import
 const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const adminUser = await admin.find(); 
+    const adminUser = await User.findOne({ username: "okulfcl"  });
+    if (!adminUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
     res.json(adminUser);
   } catch (error) {
     res.status(500).json({ message: "Server error", error });
+    console.log(error)
   }
 });
 
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,29 +1,45 @@
 import "./Login.scss";
 import loginImg from "/aaSchool.jpg";
-
+import { useEffect, useState } from "react";
 
 const Login = () => {
-    return(
-      <>
-        <div className="login-container">
-             <div className="login-img-container">
-                <img src={loginImg} alt="" className="login-img" />
-             </div>
-             <div className="login-form-container">
-                <h3 className="login-title"> Hoşgeldiniz </h3>
-                <div className="login-input-container">
-                    <p className="login-input-p"> Kullanıcı Adı </p>
-                    <input type="text" className="login-input" />
-                </div>
-                <div className="login-input-container">
-                    <p className="login-input-p"> Şifre </p>
-                    <input type="text" className="login-input" />
-                </div>
-                <button className="login-button"> Giriş Yap </button>
-             </div>
+
+  const [adminUser,setAdminUser] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/auth")
+      .then((res) => res.json())
+      .then((data) => {
+        setAdminUser(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching events:", error);
+
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="login-container">
+        <div className="login-img-container">
+          <img src={loginImg} alt="" className="login-img" />
         </div>
-      </>
-    )
+        <div className="login-form-container">
+          <h3 className="login-title"> Hoşgeldiniz </h3>
+          <div className="login-input-container">
+            <p className="login-input-p"> Kullanıcı Adı </p>
+            <input type="text" className="login-input" />
+          </div>
+          <div className="login-input-container">
+            <p className="login-input-p"> Şifre </p>
+            <input type="text" className="login-input" />
+          </div>
+          <button className="login-button"> Giriş Yap </button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Login;
